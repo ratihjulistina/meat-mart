de96828ea5eaf6b725184535bdfef841bf866926
Ratih Julistina <ratihjulistina@gmail.com>
2025-05-27 13:09:36 +0700

    fix: product list fix bug

--- apps/web/src/components/ProductList.tsx
+++ apps/web/src/components/ProductList.tsx
@@ -30,6 +30,15 @@ export const ProductList = () => {
     try {
       const lastCoords = localStorage.getItem('lastCoords');
       if (!lastCoords) {
+        const response = await getProductBasedLoc(
+          `products/from-nearest-store?lat=${`-6.1754024`}&lng=${`106.8271691649727`}&page=${page}&limit=10`,
+        );
+        const data = await response.json();
+        setProductData(data.data.data);
+
+        setTotalPages(Math.ceil(data.data.total / 10));
+        setNearestStore(data.data.store);
+
         console.log('No coordinates found');
         return;
       }
@@ -40,10 +49,19 @@ export const ProductList = () => {
         `products/from-nearest-store?lat=${lat}&lng=${lng}&page=${page}&limit=10`,
       );
       const data = await response.json();
-
+      console.log('PRODUCT DATA======', data);
       if (data.data) {
         setProductData(data.data.data);
-        //   console.log('PRODUCT DATA======', setProductData);
+
+        setTotalPages(Math.ceil(data.data.total / 10));
+        setNearestStore(data.data.store);
+      } else {
+        const response = await getProductBasedLoc(
+          `products/from-nearest-store?lat=${`-6.1754024`}&lng=${`106.8271691649727`}&page=${page}&limit=10`,
+        );
+        const data = await response.json();
+        setProductData(data.data.data);
+
         setTotalPages(Math.ceil(data.data.total / 10));
         setNearestStore(data.data.store);
       }
